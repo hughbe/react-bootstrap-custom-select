ca4ba7f6039801901ad707e8cd20557e5ac0bc5e
Hugh Bellamy <hughbellars@gmail.com>
2017-07-18 16:19:37 +0700

    Don't wrap in form group

--- src/lib/CustomSelect.js
+++ src/lib/CustomSelect.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const CustomSelect = ({values, valueSelector, ...rest}) => {
+const CustomSelect = ({values, valueSelector, defaultValue, ...rest}) => {
   const actualValueSelector = valueSelector || (v => v);
 
-  <div className="form-group">
-    <select className="form-control custom-select" {...rest}>
+  return (
+    <select className="form-control custom-select" defaultValue={defaultValue} {...rest}>
       {defaultValue && <option>{defaultValue}</option>}
       {values && values.map(v => {
         const value = actualValueSelector(v);
         return <option key={value}>{value}</option>
       })}
     </select>
-  </div>
+  );
 };
 export default CustomSelect;
